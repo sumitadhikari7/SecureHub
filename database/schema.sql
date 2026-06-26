13c9aae18b47752b376bd9cf33888362f39a85d5
Sumit Adhikari <sumitadhikari972@gmail.com>
2026-06-26 21:30:19 +0545

    Added auctions table in the database

--- database/schema.sql
+++ database/schema.sql
@@ -15,3 +15,19 @@ CREATE TABLE IF NOT EXISTS admins (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS auctions (
+    auction_id SERIAL PRIMARY KEY,
+    seller_id INT NOT NULL,
+    winner_id INT NULL, 
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    starting_price NUMERIC(12, 2) NOT NULL CHECK (starting_price >= 0),
+    current_price NUMERIC(12, 2) NOT NULL DEFAULT 0.00,
+    start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    end_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    status VARCHAR(50) DEFAULT 'upcoming', -- upcoming, active, closed
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_auction_seller FOREIGN KEY (seller_id) REFERENCES users(user_id) ON DELETE RESTRICT,
+    CONSTRAINT fk_auction_winner FOREIGN KEY (winner_id) REFERENCES users(user_id) ON DELETE SET NULL
+);
