84195d362b0a6512c7f6bc90b19bbefed2ba4df5
Prarabdha Wagle <wagleprarabdha17@gmail.com>
2026-07-09 11:36:57 +0545

    Made changes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard";
 import Authentication from "./pages/Authentication";
+import CreateAuction from "./pages/CreateAuction";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Authentication />}/>
       <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/create-auction" element={<CreateAuction />}/>
     </Routes>
   );
 }
