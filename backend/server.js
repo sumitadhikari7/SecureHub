70d0ed48b34cf56ec4bc578a18ad835688fa0b87
Sumit Adhikari <sumitadhikari972@gmail.com>
2026-07-11 22:42:09 +0545

    DB Configuration

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,12 @@ const app = express();
 // 1. Middleware setup
 app.use(cors());
 app.use(express.json());
+
+// 2. Database configuration
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT,
+});
