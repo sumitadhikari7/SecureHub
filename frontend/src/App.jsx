69c0c4b66fa42be15303ae93e11313a7badc63d7
Prarabdha Wagle <wagleprarabdha17@gmail.com>
2026-07-12 16:19:35 +0545

    Routing done for browse auction page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard";
 import Authentication from "./pages/Authentication";
 import CreateAuction from "./pages/CreateAuction";
+import BrowseAuction from "./pages/BrowseAuction";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Authentication />}/>
       <Route path="/dashboard" element={<Dashboard />}/>
       <Route path="/create-auction" element={<CreateAuction />}/>
+      <Route path="/create-auction" element={<CreateAuction />}/>
+      <Route path="/browse-auction" element={<BrowseAuction />}/>
     </Routes>
   );
 }
