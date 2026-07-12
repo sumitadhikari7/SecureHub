534d4745feb9a5dd44001dff95e14cf6d04df183
Prarabdha Wagle <wagleprarabdha17@gmail.com>
2026-07-12 19:35:10 +0545

    create function profile

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  const [profileImage, setProfileImage] = useState(null);
+
+  const user = {
+    fullName: "John Doe",
+    username: "johndoe23",
+    email: "johndoe@example.com",
+    phone: "+977 9812345678",
+    address: "Kathmandu, Nepal",
+    dob: "12 Aug 1998",
+    password: "••••••••••"
+  };
