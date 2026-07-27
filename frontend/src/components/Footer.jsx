ac15a68ba33baddb0e2e2981bd6ff5589d24fb5b
Prarabdha Wagle <wagleprarabdha17@gmail.com>
2026-07-27 19:03:00 +0545

    create support links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,15 @@
 function Footer() {
-    
+    return(
+        <footer className="site-footer">
+            <div className="footer-grid">
+                <div className="footer-col">
+                <h4>Support Links</h4>
+                    <ul>
+                        <li><a href="/about">About Us</a></li>
+                    </ul>
+                </div>
+        </footer>
+
+
+    )
 }
