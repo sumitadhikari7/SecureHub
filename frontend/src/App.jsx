1abb5ae84b696a59b75bfb81389450dd1054231e
Sumit Adhikari <sumitadhikari972@gmail.com>
2026-07-30 19:21:51 +0545

    Updated App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,46 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Authentication from "./pages/Authentication";
 import CreateAuction from "./pages/CreateAuction";
 import BrowseAuction from "./pages/BrowseAuction";
 import Profile from "./pages/Profile";
 import MyBids from "./pages/MyBids";
+import AuctionDetails from "./pages/AuctionDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Authentication />}/>
-      <Route path="/dashboard" element={<Dashboard />}/>
-      <Route path="/create-auction" element={<CreateAuction />}/>
-      <Route path="/create-auction" element={<CreateAuction />}/>
-      <Route path="/browse-auction" element={<BrowseAuction />}/>
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/my-bids" element={<MyBids />}/>
+      {/* Public Routes */}
+      <Route path="/" element={<Authentication />} />
+      <Route path="/login" element={<Authentication />} />
+      <Route path="/browse-auction" element={<BrowseAuction />} />
+      <Route path="/auction/:id" element={<AuctionDetails />} />
+
+      {/* Protected Routes */}
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/create-auction" element={
+        <ProtectedRoute>
+          <CreateAuction />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/profile" element={
+        <ProtectedRoute>
+          <Profile />
+        </ProtectedRoute>
+      } />
+      
+      <Route path="/my-bids" element={
+        <ProtectedRoute>
+          <MyBids />
+        </ProtectedRoute>
+      } />
     </Routes>
   );
 }
