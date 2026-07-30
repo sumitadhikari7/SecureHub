441f98cf65cfbc8cb7b20bbf0b55979556b29ddc
Prarabdha Wagle <wagleprarabdha17@gmail.com>
2026-07-30 16:39:10 +0545

    import styling for admin authentication

--- frontend/src/pages/AdminAuthentiction.jsx
+++ frontend/src/pages/AdminAuthentiction.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import './AdminAuthentication.css'
 function AdminAuthentication() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
